af339082a59ab43fc597327ecab1fccf6e822a6e
shrutinandan <shrutinandan31@gmail.com>
2021-06-27 17:38:32 +0530

    created randomgenerator

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-import * as React from 'react'
+import { useState } from "react";
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState({
-    counter: 0
-  })
+function useRandomNumberGenerator() {
+  const [randomNumber, setRandomNum] = useState(1);
+ 
+  // function to get the random number bewtween 1 to 100 
+  const getRandomNumber = () => {
+    const min = 1;
+    const max = 100;
+    setRandomNum(Math.floor(Math.random() * (max - min + 1) + min));
+  };
+
+  // funtion to configure init
+  const initValue = () => {
+    setRandomNum(1);
+  };
+  return [randomNumber, getRandomNumber, initValue];
+}
+export default useRandomNumberGenerator;
+
+// counter hook
+/* export const useMyHook = () => {
+  let [{ counter }, setState] = React.useState({
+    counter: 0,
+  });
 
   React.useEffect(() => {
     let interval = window.setInterval(() => {
-      counter++
-      setState({counter})
-    }, 1000)
+      counter++;
+      setState({ counter });
+    }, 1000);
     return () => {
-      window.clearInterval(interval)
-    }
-  }, [])
+      window.clearInterval(interval);
+    };
+  }, []);
 
-  return counter
-}
+  return counter;
+};*/
